7c4a21e6b987aa343393bf63cd9248f6a0baa382
Jagadeesh CH <jagadeesh11.testing@gmail.com>
2026-02-25 13:02:17 +0530

    new patient registration

--- tests/newpatient.spec.ts
+++ tests/newpatient.spec.ts
@@ -8,21 +8,25 @@ test('New patient test', async ({ page }) => {
     await page.getByRole('button',{name:'Login'}).click()   
     //new patient registration
     await page.locator(".newpatient").click()
-    await expect(page).toHaveURL("http://localhost/patients/create");
-
-    // Patient details Adding
-    await page.locator('#ownername').fill("Mahesh")
-    await page.locator('#name').fill('browney')
+    await expect(page).toHaveURL("http://localhost/patients/create");    // Patient details Adding
+    await page.locator('#ownername').fill("Rocky")
+    await page.locator('#name').fill('Rocky')
     await page.locator('#address').fill("123 main street, fine lane lane")
-    await page.locator("#mobile").fill("458225554")
-    await page.locator("#email").fill("harish@gmail.com")    
+    await page.locator("#mobile").fill("458225589")
+    await page.locator("#email").fill("black@gmail.com")    
     //await page.locator('#select2-species_id-container').selectOption("Pamerian")
+    //Species dropdown
+    await page.locator("#select2-species_id-container").click()
+    await page.locator(".select2-search__field").fill("black dog")
+    await page.locator('.select2-results__option', { hasText: '(new)' }).click();
+    //await page.locator('li', { hasText: '(new)' }).click()
+    //Gender dropdown
     await page.locator('#gender').selectOption("Male")  
     //await page.locator('#age').fill("03031993")
-    await page.locator('#breed').fill("golden retriever")
-    await page.locator('#select2-feeding_pattern-container').click()
-    await page.locator('li:has-text("Pet Food")').click()
+    await page.locator('#breed').fill("black german dog")
+    
+    //Drpdown
+    await page.locator('#feeding_pattern').selectOption("Mixed Food")
     await page.getByRole('button',{name: 'Register Patient'}).click()   
-    //await expect(page.locator("h4")).toHaveText("Patients List")
     await expect(page.getByRole('heading', { name: 'Patients List' })).toBeVisible();
 })
